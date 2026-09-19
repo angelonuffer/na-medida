df9a009d9dc448ebaa6ee9b1e94199bee042fd0b
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2026-09-19 19:51:37 +0000

    feat: atualiza configuração do Playwright para usar o comando de pré-visualização

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     headless: true
   },
   webServer: {
-    command: 'npm run dev -- --host 127.0.0.1 --port 4173',
+    command: 'npm run preview -- --host 127.0.0.1 --port 4173',
     url: 'http://127.0.0.1:4173',
     reuseExistingServer: true
   }
